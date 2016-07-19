d6cdafcef0fc9e77cd88e0842d50bc8eed104ab6
icemiliang <icemiliang@gmail.com>
2016-07-18 23:17:53 -0400

    instruction added and minor changes

--- ParsingXML/ParsingXML.py
+++ ParsingXML/ParsingXML.py
@@ -2,6 +2,12 @@ import xml.etree.ElementTree
 import glob
 import sys
 
+if len(sys.argv) < 5:
+    print('Usage:');
+    print('  python ParsingXML.py -folder input_path output_file trait_1 trait_2 ...');
+    print('  python ParsingXML.py -file input_file output_file trait_1 trait_2 ...');
+    sys.exit()
+
 output=list();
 open(sys.argv[3], 'w').close()
 
@@ -19,9 +25,12 @@ def parse(filename):
                     count=1;
                     output.insert(i,child.text);
         if count==0:
-            output.insert(i,'Trait not found in the given data');
+            output.insert(i,'N/A');
     outputfile=open(sys.argv[3],"a+");
     for i in range(len(output)):
+        if i==len(output)-1:
+            outputfile.write(output[i]);
+        else:
             outputfile.write(output[i]+',');
     outputfile.write('\n');
     outputfile.close();
